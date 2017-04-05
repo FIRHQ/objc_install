62c3abe653b84f1f9b2e43056bde6a2f1a025dd4
yang.guo <gy@fir.im>
2017-04-05 23:28:50 +0800

    remove pod lock file for library conflict

--- run.sh
+++ run.sh
@@ -1,11 +1,21 @@
 cd $FLOW_CURRENT_PROJECT_PATH
 
+remove_lockfile() {
+  podfile_lock=($(find ./ -maxdepth 1 -name "Podfile.lock"))
+  if [ -n $podfile_lock ]; then
+    mv $podfile_lock Podfile.bk.lock
+  fi
+}
+
 if [ -f Podfile ];then
     cp Podfile Podfile.bak
     sed -i -e 's/^[[:space:]]*source/#source/g' Podfile
     export http_proxy=http://172.16.152.110:8123
     export https_proxy=$http_proxy
+    
+    remove_lockfile
     flow_cmd "pod install --no-repo-update"
+    
     unset http_proxy
     unset https_proxy
 fi

6c84bcf0fe902e185d069762a6551f7f82400cce
Mayi <my3157@hotmail.com>
2017-04-25 16:07:45 +0800

    update to /cache

--- run.sh
+++ run.sh
@@ -12,18 +12,8 @@ if [ -f Podfile ];then
     sed -i -e 's/^[[:space:]]*source/#source/g' Podfile
     source /usr/local/bin/proxy.sh
     # export https_proxy=$http_proxy
-#    remove_lockfile
+    # remove_lockfile
     flow_cmd "pod install --no-repo-update"    
     source /usr/local/bin/unproxy.sh
 fi
-tar -zcf Pods.tar.gz Pods
 
-ftp -n -i >> ftp.log <<EOF
-open 172.16.152.110
-user flow flow.ci
-cd flow
-mkdir ${FLOW_USER_ID}
-mkdir ${FLOW_USER_ID}/${FLOW_PROJECT_ID}
-put Pods.tar.gz ${FLOW_USER_ID}/${FLOW_PROJECT_ID}/Pods.tar.gz
-bye
-EOF
